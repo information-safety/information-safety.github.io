0754648ca8efd37963216fc7f2013a20411dc288
jabenninghoff <jbenninghoff@mac.com>
2016-11-19 16:40:50 -0600

    Switch to native docker

--- docker-pages.sh
+++ docker-pages.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # load site in a docker container that mimics GitHub Pages using jekyll/docker
-# use "sh docker-pages.sh jekyll serve --force_polling --drafts" to enable drafts and polling
-eval $(docker-machine env default)
-docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll -it -p $(docker-machine ip `docker-machine active`):4000:4000 jekyll/jekyll:pages $*
+# use "sh docker-pages.sh jekyll serve --drafts" to enable drafts
+docker run --rm --name jekyll --volume=$(pwd):/srv/jekyll -it -p 4000:4000 jekyll/jekyll:pages $*
