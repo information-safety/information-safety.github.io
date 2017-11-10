778fb9125cd040e75a09042b803fd1193703eb68
jabenninghoff <jbenninghoff@mac.com>
2017-11-09 19:38:04 -0600

    remove broken docker script

--- docker-pages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# load site in a docker container that mimics GitHub Pages using jekyll/docker
-# use "sh docker-pages.sh jekyll serve --drafts" to enable drafts
-docker run --rm --name jekyll --volume=$(pwd):/srv/jekyll -it -p 4000:4000 jekyll/jekyll:pages $*
