436c4c989589e8c00db2b02564e296588aee4488
John Benninghoff <jbenninghoff@mac.com>
2024-08-09 16:06:57 -0500

    Import framework changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,11 @@
 # use rbenv to install base ruby stack (ruby, gem, bundler)
 set -ex # halt script on error, echo on
 
-rbenv install `cat .ruby-version`
+# NOTE: rbenv not needed for current versions used with `brew rbenv-sync`
+# rbenv install "$(cat .ruby-version)"
+
+# per-project gems, see https://guilhermesimoes.github.io/blog/installing-gems-per-project-directory
+bundle config --local path vendor/bundle
+bundle config --local with ci
 bundle install
 bundle package
